df8a344a037f35072936bd42541a5ed17932a67e
José Roberto Estupinián <jose-roberto.estupinian@sievo.com>
2025-04-30 15:43:39 +0300

    neovime setup for ruby and python, without the lsp servers

---
+++ confing_nvim/lua/vim-options.lua
@@ -0,0 +1,19 @@
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=4 smarttab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=0")
+vim.cmd("set relativenumber")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set nowrap")
+vim.g.mapleader = ","
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
